fec1b0620bf0e8614ab8af7afb96f2f2a7442866
Nektro <nektromc@gmail.com>
2017-12-22 03:02:40 -0500

    move nomodule.js to the root for use in other apps

--- news/nomodule.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    document.querySelectorAll('header')[0].style.display = "none";
-    document.querySelectorAll('main')[0].style.display = "none";
-    document.querySelectorAll('footer')[0].style.display = "none";
-    document.body.style.margin = "1em";
-    document.body.innerHTML += '<p>This browser does not support ES6 Modules</p>';
-    document.body.innerHTML += '<p>Consider upgrading to Edge 16+, Chrome 61+, Safari 10.1+, or iOS Safari 11+</p>'
-    document.body.innerHTML += '<p>See <a href="https://caniuse.com/#feat=es6-module">caniuse.com</a> more more info.</p>';
-});
