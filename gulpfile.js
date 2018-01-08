9806da17a4037a63597281de0a0a817ff42069cf
Nektro <nektromc@gmail.com>
2018-01-08 14:06:37 -0500

    update apps to use gulp include

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@ const gulp = require('gulp');
 const gih = require("gulp-include-html");
 
 gulp.task('default', function() {
-    gulp.src('./src/**/*.*')
-    //.pipe(gih())
+    gulp.src('./src/**/*.html')
+    .pipe(gih())
     //.pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('bin'));
+
+    gulp.src('./src/**/*.{css,js,png}')
     .pipe(gulp.dest('bin'));
 });
