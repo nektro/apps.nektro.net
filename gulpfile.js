06a69a0b8b72efec8640fa4b077f0711a610bb62
Nektro <nektromc@gmail.com>
2018-01-09 12:01:28 -0500

    fix timing on gulp output

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const gulp = require('gulp');
 const gih = require("gulp-include-html");
 
 gulp.task('html', function() {
-    gulp.src('./src/**/*.{html,json}')
+    return gulp.src('./src/**/*.{html,json}')
     .pipe(gih())
     .pipe(gulp.dest('bin'));
 });
 
 gulp.task('static', function() {
-    gulp.src('./src/**/*.{css,js,png}')
+    return gulp.src('./src/**/*.{css,js,png}')
     .pipe(gulp.dest('bin'));
 });
 
