1b0a3322b9d728120b5db77b39daf89f73aa3874
Nektro <nektromc@gmail.com>
2018-03-07 23:52:57 -0500

    serivce worker : change approved domains to cached domains

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 /**
  * apps.nektro.net Service Worker
  */
-const APPROVED_DOMAINS = [
+const CACHED_DOMAINS = [
     'unpkg.com',
     'fonts.googleapis.com',
     'fonts.gstatic.com',
@@ -13,7 +13,7 @@ self.addEventListener('fetch', event => {
         caches.match(event.request).then(response => {
             const requestUrl = new URL(event.request.url);
             if (navigator.onLine === true) {
-                if (location.host === requestUrl.host || APPROVED_DOMAINS.includes(requestUrl.host)) {
+                if (location.host === requestUrl.host || CACHED_DOMAINS.includes(requestUrl.host)) {
                     caches.open('static-v1').then(cache => {
                         fetch(event.request).then(response2 => {
                             cache.put(event.request, response2);
