cd80fb83ac06c08d8ade9ae228ff64a8b8201ebb
Sean Denny <nektromc@gmail.com>
2018-05-15 23:02:48 -0400

    index :: add fix function for apps with `_` in ID

--- src/app.js
+++ src/app.js
@@ -13,12 +13,15 @@ function dcTN(string) {
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+function idTOname(string) {
+    return string.split('_').map(v => capitalize(v), '').join(' ');
+}
 //
 customElements.define('x-app', class extends HTMLElement {
     constructor() {
         super();
         let id = this.dataset.id;
-        let name = capitalize(id);
+        let name = idTOname(id);
         this.appendChild(create_element('a', [['class','card'],['href',`./${id}/`]], [
             create_element('img', [['src',`./${id}/dusk.png`],['alt',`Logo`]]),
             create_element('h3', undefined, [ dcTN(name) ])
