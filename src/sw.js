049c7158b7082b4b7054f9077c29178a7e75b234
Sean Denny <nektromc@gmail.com>
2018-06-08 19:35:36 -0400

    update service worker for eslint

--- src/sw.js
+++ src/sw.js
@@ -2,19 +2,19 @@
  * apps.nektro.net Service Worker
  */
 const CACHED_DOMAINS = [
-    'unpkg.com',
-    'fonts.googleapis.com',
-    'fonts.gstatic.com',
-    'cdn.rawgit.com'
+    "unpkg.com",
+    "fonts.googleapis.com",
+    "fonts.gstatic.com",
+    "cdn.rawgit.com"
 ];
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             const requestUrl = new URL(event.request.url);
             if (navigator.onLine === true) {
                 if (location.host === requestUrl.host || CACHED_DOMAINS.includes(requestUrl.host)) {
-                    caches.open('static-v1').then(cache => {
+                    caches.open("static-v1").then(cache => {
                         fetch(event.request).then(response2 => {
                             cache.put(event.request, response2);
                         });
